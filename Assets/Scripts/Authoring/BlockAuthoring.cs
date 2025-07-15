5bc27d92712ef00a1977f5b4ba5ed93834008b62
WizMager <wizmager@gmailcom>
2025-07-15 21:11:45 +0300

    -create field, create block prefab and some scripts

---
+++ Assets/Scripts/Authoring/BlockAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring
+{
+    public class BlockAuthoring : MonoBehaviour
+    {
+        private class BlockAuthoringBaker : Baker<BlockAuthoring>
+        {
+            public override void Bake(BlockAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<BlockTagComponent>(entity);
+            }
+        }
+    }
+}
